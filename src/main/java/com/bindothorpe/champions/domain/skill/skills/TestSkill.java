a33045aaeaca06366391628aa11a3436a3a3b86d
bindothorpe <bindo.thorpe@student.hogent.be>
2023-06-07 15:48:51 +0200

    Fixed a bug that caused the skill description to duplicate

--- src/main/java/com/bindothorpe/champions/domain/skill/skills/TestSkill.java
+++ src/main/java/com/bindothorpe/champions/domain/skill/skills/TestSkill.java
@@ -40,12 +40,13 @@ public class TestSkill extends Skill {
 
     @Override
     public List<Component> getDescription(int skillLevel) {
-        description.add(Component.text("This is a test skill").color(NamedTextColor.GRAY));
-        description.add(Component.text("Right-click").color(NamedTextColor.YELLOW)
-                .append(Component.text(" to heal ").color(NamedTextColor.GRAY))
-                .append(Component.text(healing.get(Math.max(skillLevel - 1, 0))).color(NamedTextColor.GREEN))
-                .append(Component.text(" hp").color(NamedTextColor.GREEN)));
-
+        if(description.isEmpty()) {
+            description.add(Component.text("This is a test skill").color(NamedTextColor.GRAY));
+            description.add(Component.text("Right-click").color(NamedTextColor.YELLOW)
+                    .append(Component.text(" to heal ").color(NamedTextColor.GRAY))
+                    .append(Component.text(healing.get(Math.max(skillLevel - 1, 0))).color(NamedTextColor.GREEN))
+                    .append(Component.text(" hp").color(NamedTextColor.GREEN)));
+        }
         return description;
     }
 }
