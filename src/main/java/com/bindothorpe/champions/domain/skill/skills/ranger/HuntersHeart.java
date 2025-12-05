39691bed31de4d05f88b0cd51f64fd2471dc2c0e
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2025-12-05 17:16:42 +0100

    Fixed hunters heart passive

--- src/main/java/com/bindothorpe/champions/domain/skill/skills/ranger/HuntersHeart.java
+++ src/main/java/com/bindothorpe/champions/domain/skill/skills/ranger/HuntersHeart.java
@@ -12,53 +12,86 @@ import jdk.jfr.EventType;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.EntityEffect;
+import org.bukkit.Particle;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitTask;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class HuntersHeart extends Skill {
 
-    private final List<Double> healing = Arrays.asList(1.0, 2.0, 4.0);
-    private static final double DURATION = 5.0;
+    private static final double HEALING = 1.0D;
+    private final Set<UUID> active = new HashSet<>();
+    private final Map<UUID, Integer> taskIdMap = new HashMap<>();
+    private final List<Double> durationBeforeHeal = List.of(12D, 10D, 8D);
+    private final List<Double> intervalBetweenHeal = List.of(2D, 1.5D, 1D);
+    private static final double INTERVAL = 2.0;
     public HuntersHeart(DomainController dc) {
         super(dc, SkillId.HUNTERS_HEART, SkillType.PASSIVE_B, ClassType.RANGER, "Hunter's Heart", null, 3, 1);
     }
 
+
     @EventHandler
     public void onUpdate(UpdateEvent event) {
-        if(!event.getUpdateType().equals(UpdateType.TWO_SECOND))
+        if(!event.getUpdateType().equals(UpdateType.RAPID))
             return;
 
         for (UUID uuid : getUsers()) {
-            if(dc.getCombatLogger().hasTakenDamageWithinDuration(uuid, DURATION))
-                continue;
+            if(dc.getCombatLogger().hasTakenDamageWithinDuration(uuid, durationBeforeHeal.get(getSkillLevel(uuid) - 1))){
+                if(!active.contains(uuid)) continue;
 
-            Player player = Bukkit.getPlayer(uuid);
+                active.remove(uuid);
+                int taskId = taskIdMap.remove(uuid);
+                dc.getPlugin().getServer().getScheduler().cancelTask(taskId);
 
-            if(player.getHealth() == player.getMaxHealth())
-                continue;
+            } else {
+                if(active.contains(uuid)) continue;
+                Player player = Bukkit.getPlayer(uuid);
+                if(player == null) continue;;
+                if(player.getHealth() == Objects.requireNonNull(player.getAttribute(Attribute.MAX_HEALTH)).getValue()) continue;
+                active.add(uuid);
+                BukkitTask task =  dc.getPlugin().getServer().getScheduler().runTaskTimer(
+                        dc.getPlugin(),
+                        ()-> performHuntersHeart(uuid),
+                        0L,
+                        (long) (intervalBetweenHeal.get(getSkillLevel(uuid) - 1) * 20L)
+                );
+                taskIdMap.put(uuid, task.getTaskId());
 
-            player.setHealth(Math.min(player.getHealth() + healing.get(getSkillLevel(uuid) - 1), player.getMaxHealth()));
-
+            }
         }
 
+    }
+
+    private void performHuntersHeart(UUID uuid) {
+        Player player = Bukkit.getPlayer(uuid);
+        if(player == null) return;
+        player.heal(HEALING);
+        player.getWorld().spawnParticle(Particle.HEART, player.getEyeLocation(), 1);
+        if(player.getHealth() == Objects.requireNonNull(player.getAttribute(Attribute.MAX_HEALTH)).getValue()) {
+            active.remove(uuid);
+            int taskId = taskIdMap.remove(uuid);
+            dc.getPlugin().getServer().getScheduler().cancelTask(taskId);
+        }
+//        player.setHealth(Math.min(player.getHealth() + healing.get(getSkillLevel(uuid) - 1), player.getMaxHealth()));
     }
 
     @Override
     public List<Component> getDescription(int skillLevel) {
         List<Component> lore = new ArrayList<>();
         lore.add(Component.text("Passive: ").color(NamedTextColor.WHITE)
-                        .append(Component.text("Heal for").color(NamedTextColor.GRAY)));
-        lore.add(ComponentUtil.skillLevelValues(skillLevel, healing, NamedTextColor.YELLOW)
-                .append(Component.text(" health every").color(NamedTextColor.GRAY)));
-        lore.add(Component.text("2.0 seconds, after not taking").color(NamedTextColor.GRAY));
+                        .append(Component.text("Heal half a heart").color(NamedTextColor.GRAY)));
+        lore.add(Component.text("every ").color(NamedTextColor.GRAY)
+                    .append(ComponentUtil.skillLevelValues(skillLevel, intervalBetweenHeal, NamedTextColor.YELLOW)));
+        lore.add(Component.text("seconds, after not taking").color(NamedTextColor.GRAY));
         lore.add(Component.text("damage for ").color(NamedTextColor.GRAY)
-                .append(Component.text(DURATION).color(NamedTextColor.GRAY))
-                .append(Component.text(" seconds").color(NamedTextColor.GRAY)));
+                .append(ComponentUtil.skillLevelValues(skillLevel, durationBeforeHeal, NamedTextColor.YELLOW)));
+        lore.add(Component.text("seconds.").color(NamedTextColor.GRAY));
         return lore;
     }
 }
