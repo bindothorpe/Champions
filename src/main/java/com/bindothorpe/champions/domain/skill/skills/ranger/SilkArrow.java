9fedd3fa548637588d505bdb65e66c3441d65c1f
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2025-12-05 17:17:18 +0100

    Fixed bug that allowed you to activate silk arrow skill by left-clicking with any item

--- src/main/java/com/bindothorpe/champions/domain/skill/skills/ranger/SilkArrow.java
+++ src/main/java/com/bindothorpe/champions/domain/skill/skills/ranger/SilkArrow.java
@@ -5,39 +5,35 @@ import com.bindothorpe.champions.domain.build.ClassType;
 import com.bindothorpe.champions.domain.skill.Skill;
 import com.bindothorpe.champions.domain.skill.SkillId;
 import com.bindothorpe.champions.domain.skill.SkillType;
-import com.bindothorpe.champions.domain.sound.CustomSound;
 import com.bindothorpe.champions.events.interact.PlayerLeftClickEvent;
 import com.bindothorpe.champions.events.update.UpdateEvent;
 import com.bindothorpe.champions.events.update.UpdateType;
-import com.bindothorpe.champions.util.ShapeUtil;
+import com.bindothorpe.champions.util.ComponentUtil;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.Particle;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.metadata.FixedMetadataValue;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class SilkArrow extends Skill {
 
+    private static final double DURATION = 10.0D;
+
     private final Set<UUID> primed = new HashSet<>();
     private final Set<Arrow> particleTrail = new HashSet<>();
-    private final List<Double> duration = Arrays.asList(3.0, 4.0, 5.0);
 
 
     public SilkArrow(DomainController dc) {
-        super(dc, SkillId.SILK_ARROW, SkillType.BOW, ClassType.RANGER, "Silk Arrow", Arrays.asList(10.0), 1, 2);
+        super(dc, SkillId.SILK_ARROW, SkillType.BOW, ClassType.RANGER, "Silk Arrow", List.of(10.0), 1, 2);
     }
 
     @EventHandler
@@ -53,15 +49,11 @@ public class SilkArrow extends Skill {
 
     @EventHandler
     public void onArrowLaunch(ProjectileLaunchEvent event) {
-        if (!(event.getEntity() instanceof Arrow))
+        if (!(event.getEntity() instanceof Arrow arrow))
             return;
 
-        Arrow arrow = (Arrow) event.getEntity();
-
-        if (!(arrow.getShooter() instanceof Player))
+        if (!(arrow.getShooter() instanceof Player player))
             return;
-
-        Player player = (Player) arrow.getShooter();
 
         if (!primed.contains(player.getUniqueId()))
             return;
@@ -76,19 +68,15 @@ public class SilkArrow extends Skill {
 
     @EventHandler
     public void onArrowHit(ProjectileHitEvent event) {
-        if (!(event.getEntity() instanceof Arrow))
+        if (!(event.getEntity() instanceof Arrow arrow))
             return;
-
-        Arrow arrow = (Arrow) event.getEntity();
 
         if (!arrow.hasMetadata("silk"))
             return;
 
-        if(!(arrow.getShooter() instanceof Player)) {
+        if(!(arrow.getShooter() instanceof Player player)) {
             return;
         }
-
-        Player player = (Player) arrow.getShooter();
 
         particleTrail.remove(arrow);
 
@@ -97,20 +85,20 @@ public class SilkArrow extends Skill {
     }
 
     private void performSilk(Player player, Arrow arrow, Entity hit) {
-        List<Vector> vectors = ShapeUtil.sphere(3).stream().sorted((v1, v2) -> v2.getBlockY() - v1.getBlockY()).collect(Collectors.toList());
-
-        Location loc = hit != null ? hit.getLocation() : arrow.getLocation();
-
-        for(int i = 0; i < vectors.size(); i++) {
-            Vector v = vectors.get(i);
-            double finalDuration = duration.get(getSkillLevel(player.getUniqueId())) + (i / 500D);
-
-            dc.getTemporaryBlockManager().spawnTemporaryBlock(
-                    loc.clone().add(v),
-                    Material.COBWEB,
-                    finalDuration
-            );
-        }
+//        List<Vector> vectors = ShapeUtil.sphere(1).stream().sorted((v1, v2) -> v2.getBlockY() - v1.getBlockY()).collect(Collectors.toList());
+//
+//        Location loc = hit != null ? hit.getLocation() : arrow.getLocation();
+//
+//        for(int i = 0; i < vectors.size(); i++) {
+//            Vector v = vectors.get(i);
+//            double finalDuration = duration.get(getSkillLevel(player.getUniqueId())) + (i / 500D);
+//
+//            dc.getTemporaryBlockManager().spawnTemporaryBlock(
+//                    loc.clone().add(v),
+//                    Material.COBWEB,
+//                    finalDuration
+//            );
+//        }
 
         arrow.remove();
     }
@@ -123,22 +111,56 @@ public class SilkArrow extends Skill {
         for (Arrow arrow : particleTrail) {
             Location loc = arrow.getLocation();
 
-            if(!(arrow.getShooter() instanceof Player)) {
+            if(!(arrow.getShooter() instanceof Player player)) {
                 return;
             }
-
-            Player player = (Player) arrow.getShooter();
 
             if(player.getLocation().distance(arrow.getLocation()) <= 2) {
                 return;
             }
 
-            dc.getTemporaryBlockManager().spawnTemporaryBlock(loc, Material.COBWEB, duration.get(getSkillLevel(player.getUniqueId()) - 1));
+            dc.getTemporaryBlockManager().spawnTemporaryBlock(loc, Material.COBWEB, DURATION);
         }
+    }
+
+
+    @Override
+    protected boolean canUseHook(UUID uuid, Event event) {
+        if (!(event instanceof PlayerLeftClickEvent))
+            return false;
+
+        PlayerLeftClickEvent e = (PlayerLeftClickEvent) event;
+
+
+        if (dc.getTeamManager().getTeamFromEntity(e.getPlayer()) == null) {
+            return false;
+        }
+
+
+        if (!e.isBow())
+            return false;
+
+
+        if (primed.contains(uuid))
+            return false;
+
+
+        return super.canUseHook(uuid, event);
     }
 
     @Override
     public List<Component> getDescription(int skillLevel) {
-        return List.of();
+        List<Component> lore = new ArrayList<>();
+        lore.add(ComponentUtil.active()
+                .append(ComponentUtil.leftClick())
+                .append(Component.text("to prime").color(NamedTextColor.GRAY)));
+        lore.add(Component.text("your next arrow.").color(NamedTextColor.GRAY));
+        lore.add(Component.text(" "));
+        lore.add(Component.text("Your next arrow will leave").color(NamedTextColor.GRAY));
+        lore.add(Component.text("behind a trail of cobwebs").color(NamedTextColor.GRAY));
+        lore.add(Component.text("that will remain for ").color(NamedTextColor.GRAY).append(ComponentUtil.skillLevelValues(skillLevel, List.of(DURATION), NamedTextColor.YELLOW)));
+        lore.add(Component.text("seconds.").color(NamedTextColor.GRAY));
+
+        return lore;
     }
 }
